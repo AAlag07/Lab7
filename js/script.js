fa557488b5c05088dd9799f9ec8d1dcd52814791
AAlag07 <112470063+AAlag07@users.noreply.github.com>
2022-11-08 09:29:09 -0600

    added js changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-function init(){
+function init() {
 //add your javascrip between these two lines of code
-	function alertvar(){
+	function makealert() {
 			var input = document.getElementById('entryinput').value;
 			var output = document.getElementById('textoutput');
-			output.innerHTML = input;
+			document.getElementById('textoutput').innerText = input;
 			alert('Angelo Alag: ' + input);
 	}
 	
 	var vbutton = document.getElementById('entrybutton');
-			button.addEventListener('click', alertvar);
+			vbutton.addEventListener('click', alertvar);
 }
 
 
